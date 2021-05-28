e9ad4b14254baba7a18db55e01787715cfba094c
szhao23 <stevenzhao456@gmai.com>
2021-05-28 14:21:28 -0500

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './components/index.css';
+import App from './components/app.js';
  
 ReactDOM.render(<App />, document.getElementById('root'));
