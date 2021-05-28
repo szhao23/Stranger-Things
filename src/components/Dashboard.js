ca690486272b7d092dbf10e0e6a584dbdf8d965d
szhao23 <stevenzhao456@gmai.com>
2021-05-28 13:47:44 -0500

    SRC Folder

---
+++ src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+ 
+function Dashboard(props) {
+ 
+  // handle click event of logout button
+  const handleLogout = () => {    
+    props.history.push('/login');
+  }
+ 
+  return (
+    <div>
+      Welcome User!<br /><br />
+      <input type="button" onClick={handleLogout} value="Logout" />
+    </div>
+  );
+}
+ 
+export default Dashboard;
---
+++ src/components/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+ 
+import Login from './Login';
+import Dashboard from './Dashboard';
+import Home from './Home';
+ 
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div>
+          <div className="header">
+            <NavLink exact activeClassName="active" to="/">Home</NavLink>
+            <NavLink activeClassName="active" to="/login">Login</NavLink><small>(Access without token only)</small>
+            <NavLink activeClassName="active" to="/dashboard">Dashboard</NavLink><small>(Access with token only)</small>
+          </div>
+          <div className="content">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/login" component={Login} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+ 
+export default App;
